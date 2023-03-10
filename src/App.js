ae79d3e6a85d5f3e8f65298ddc576a3416cf2c79
Jhunel Peñaflorida <jhunel.penaflorida@novare.com.hk>
2023-03-10 09:04:56 +0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {Component, useState} from "react"
+
+// CSS Styles
+import './App.css'
+
+// Component
+import Card from './Card'
+const App = () => {
+   
+   
+
+    return(
+       <div className = "app-main">
+           <Card></Card>
+       </div>
+    )
+}
+export default App
---
+++ src/Card.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = ()=>{
+    return (
+    <div className="users-card">
+        <h1>User's Card</h1>
+        <div>Name: Jhunel</div>
+        <div>Age: 25</div>
+        <div>Gender: Male</div>
+    </div>
+    )
+}
+export default Card
